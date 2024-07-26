58cdefb4fd0d1d2a39aa6acdc053c5a10bbdd9c2
Sharvari Kenkar <kenkarsharvari@gmail.com>
2024-07-26 11:05:15 +0530

    Assignment4

---
+++ assignment4/Darktheme.js
@@ -0,0 +1,17 @@
+
+function swapTheme() {
+   
+    const app = document.getElementById("app")
+    const themeToggle = document.getElementById("swap")
+    if (themeToggle.classList.contains("button_day")) {
+        themeToggle.classList.remove("button_day")
+        themeToggle.classList.add("button_night")
+        app.classList.remove("day")
+        app.classList.add("night")
+    } else {
+        themeToggle.classList.add("button_day")
+        themeToggle.classList.remove("button_night")
+        app.classList.add("day")
+        app.classList.remove("night")
+    }
+}
